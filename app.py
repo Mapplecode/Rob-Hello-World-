80e52820993f0ff1a8d4bd651f7a295ad1536f42
unknown <pshkmr007@gmail.com>
2021-03-15 10:57:50 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template,request
+
+
+app=Flask(__name__)
+
+@app.route("/",methods=['GET','POST'])
+def home():
+    return "<h1>Hello World!!</h1>"
+
+@app.route("/add2number",methods=['GET','POST'])
+def addtonumber():
+    try:
+        if request.method == "POST":
+            val1 = request.form["val1"]
+            val2 = request.form["val2"]
+            val3 = int(val1) + int(val2)
+            return render_template('index.html',value=val3) 
+        return render_template('index.html')
+    except Exception as e:
+        return f"Somthing Went Wrong!!, {e}"
+
+if __name__ == "__main__":
+    app.run()
